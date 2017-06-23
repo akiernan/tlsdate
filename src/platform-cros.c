f1b34b640727bc46416a0ae912d836bc12a09a36
Daniel Erat <derat@chromium.org>
2017-06-22 23:16:17 -0600

    tlsdate: Add tests for proxy-parsing code.
    Move get_valid_hostport() and canonicalize_pac() out of
    platform-cros.c and into a new platform-cros-util.c file.
    Add a bunch of unit tests for canonicalize_pac() that get
    compiled into a new platform-cros-util_unittest executable.
    
    No functional changes.
    
    BUG=chromium:708177
    TEST=built with FEATURES=test
    
    Change-Id: Ide07f77292ee5f7fee133a3ad8b27e0afb4de800
    Reviewed-on: https://chromium-review.googlesource.com/545225
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- src/platform-cros.c
+++ src/platform-cros.c
@@ -19,6 +19,7 @@
 
 #include "src/dbus.h"
 #include "src/platform.h"
+#include "src/platform-cros-util.h"
 #include "src/tlsdate.h"
 #include "src/util.h"
 
@@ -58,107 +59,6 @@ struct platform_state
   int resolve_msg_count;
   uint32_t resolve_network_proxy_serial;
 };
-
-static
-bool
-get_valid_hostport (const char *hostport, char *out, size_t len)
-{
-  bool host = true;
-  const char *end = hostport + strlen (hostport);
-  const char *c;
-  *out = '\0';
-  /* Hosts begin with alphanumeric only. */
-  if (!isalnum (*hostport))
-    {
-      info ("Host does not start with alnum");
-      return false;
-    }
-  *out++ = *hostport;
-  for (c = hostport + 1;  c < end && len > 0; ++c, ++out, --len)
-    {
-      *out = *c;
-      if (host)
-        {
-          if (isalnum (*c) || *c == '-' || *c == '.')
-            {
-              continue;
-            }
-          if (*c == ':')
-            {
-              host = false;
-              continue;
-            }
-        }
-      else
-        {
-          if (isdigit (*c))
-            continue;
-        }
-      *out = '\0';
-      return false;
-    }
-  *out = '\0';
-  return true;
-}
-
-/* Convert PAC return format to tlsdated url format */
-/* TODO(wad) support multiple proxies when Chromium does:
- * PROXY x.x.x.x:yyyy; PROXY z.z.z.z:aaaaa
- */
-static
-void
-canonicalize_pac (const char *pac_fmt, char *proxy_url, size_t len)
-{
-  size_t type_len;
-  size_t copied = 0;
-  const char *space;
-  /* host[255]:port[6]\0 */
-  char hostport[6 + 255 + 2];
-  proxy_url[0] = '\0';
-  if (len < 1)
-    return;
-  if (!strcmp (pac_fmt, "DIRECT"))
-    {
-      return;
-    }
-  /* Find type */
-  space = strchr (pac_fmt, ' ');
-  if (!space)
-    return;
-  type_len = space - pac_fmt;
-  if (!get_valid_hostport (space + 1, hostport, sizeof (hostport)))
-    {
-      error ("invalid host:port: %s", space + 1);
-      return;
-    }
-  proxy_url[0] = '\0';
-  if (!strncmp (pac_fmt, "PROXY", type_len))
-    {
-      copied = snprintf (proxy_url, len, "http://%s", hostport);
-    }
-  else if (!strncmp (pac_fmt, "SOCKS", type_len))
-    {
-      copied = snprintf (proxy_url, len, "socks4://%s", hostport);
-    }
-  else if (!strncmp (pac_fmt, "SOCKS5", type_len))
-    {
-      copied = snprintf (proxy_url, len, "socks5://%s", hostport);
-    }
-  else if (!strncmp (pac_fmt, "HTTPS", type_len))
-    {
-      copied = snprintf (proxy_url, len, "https://%s", hostport);
-    }
-  else
-    {
-      error ("pac_fmt unmatched: '%s' %zu", pac_fmt, type_len);
-    }
-  if (copied >= len)
-    {
-      error ("canonicalize_pac: truncation '%s'", proxy_url);
-      proxy_url[0] = '\0';
-      return;
-    }
-}
 
 static
 DBusHandlerResult
